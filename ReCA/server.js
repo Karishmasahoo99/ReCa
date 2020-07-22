a0055902786cd7056a35a9cc1e9342fe98c4e7e9
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-22 20:46:06 +0530

    Update server.js

--- ReCA/server.js
+++ ReCA/server.js
@@ -1,9 +1,20 @@
 const path=require('path')
+const http=require('http')
 const express=require('express')
+const socketio=require('socket.io')
+
 const app=express()
+const server=http.createServer(app)
+const io=socketio(server)
 
 //Set Static Folder
 app.use(express.static(path.join(__dirname,'public')))
 
+//Run when client connect
+io.on('connection',socket=>{
+    console.log('New WS connection...')
+    socket.emit('message','Welcome to ChatCord')
+})
+
 const PORT=3000||process.env.PORT
-app.listen(PORT,()=>console.log(`Server started at PORT ${PORT}`))
+server.listen(PORT,()=>console.log(`Server started at PORT ${PORT}`))
