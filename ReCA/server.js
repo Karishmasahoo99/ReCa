a6e9e93eade07be57274ce1f996294da6efd3129
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-23 12:48:46 +0530

    Update server.js

--- ReCA/server.js
+++ ReCA/server.js
@@ -12,8 +12,20 @@ app.use(express.static(path.join(__dirname,'public')))
 
 //Run when client connect
 io.on('connection',socket=>{
-    console.log('New WS connection...')
     socket.emit('message','Welcome to ChatCord')
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message','A user has joined the chart')
+    
+    //Run when client disconnects
+    socket.on('disconnect',()=>{
+        io.emit('message','A user has left the chat')
+    })
+
+    //Listen for chatmessage
+    socket.on('chatMessage',msg=>{
+        io.emit('message',msg)
+    })
 })
 
 const PORT=3000||process.env.PORT
