b9ecb0acb5a9da2c845f3ddd0279ca624281512c
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-24 22:12:08 +0530

    Update server.js

--- ReCA/server.js
+++ ReCA/server.js
@@ -2,6 +2,8 @@ const path=require('path')
 const http=require('http')
 const express=require('express')
 const socketio=require('socket.io')
+const formatMessages=require('./utils/messages')
+const formatMessage = require('./utils/messages')
 
 const app=express()
 const server=http.createServer(app)
@@ -9,22 +11,23 @@ const io=socketio(server)
 
 //Set Static Folder
 app.use(express.static(path.join(__dirname,'public')))
+const botName='ChatCord Bot '
 
 //Run when client connect
 io.on('connection',socket=>{
-    socket.emit('message','Welcome to ChatCord')
+    socket.emit('message',formatMessage(botName,'Welcome to ChatCord'))
 
     //Broadcast when a user connects
-    socket.broadcast.emit('message','A user has joined the chart')
+    socket.broadcast.emit('message',formatMessage(botName,'A user has joined the chart'))
     
     //Run when client disconnects
     socket.on('disconnect',()=>{
-        io.emit('message','A user has left the chat')
+        io.emit('message',formatMessage(botName,'A user has left the chat'))
     })
 
     //Listen for chatmessage
     socket.on('chatMessage',msg=>{
-        io.emit('message',msg)
+        io.emit('message',formatMessage('User ',msg))
     })
 })
 
