12221cf33d4c0c05ca6454f0363b9d75acb620c3
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-25 18:26:45 +0530

    Update main.js

--- ReCA/public/js/main.js
+++ ReCA/public/js/main.js
@@ -1,11 +1,33 @@
 const chatForm=document.getElementById('chat-form')
+const chatMessages=document.querySelector('.chat-messages');
+const roomName=document.getElementById('room-name');
+const userList=document.getElementById('users')
+
+//Get username and room from URL
+const {username,room}=Qs.parse(location.search,{
+  ignoreQueryPrefix: true
+})
+
+console.log(username,room);
 
 const socket=io();
+
+//Join chatroom
+socket.emit('joinRoom',{username,room});
+
+//Get Room and users
+socket.on('roomUsers',({room,users})=>{
+  outputRoomName(room);
+  outputUsers(users);
+})
 
 //Message from server
 socket.on('message',message=>{
     console.log(message)
     outputMessage(message)
+
+    //Scroll Down
+    chatMessages.scrollTop=chatMessages.scrollHeight;
 })
 
 //message submit
@@ -15,15 +37,30 @@ chatForm.addEventListener('submit',(e)=>{
    
    //Emit msg to server
    socket.emit('chatMessage',msg)
+   //Clear input
+   e.target.elements.msg.value='';
+   e.target.elements.msg.focus();
 })
 
 //Output message to DOM
 function outputMessage(message){
     const div=document.createElement('div')
     div.classList.add('message')
-    div.innerHTML=`<p class="meta">Brad<span>9:12pm</span></p>
+    div.innerHTML=`<p class="meta">${message.username}<span>${message.time}</span></p>
     <p class="text">
-      ${message}
+      ${message.text}
     </p>`
     document.querySelector('.chat-messages').appendChild(div)
+}
+
+//Add room to DOM
+function outputRoomName(){
+    roomName.innerText=room;
+}
+
+//add users to DOM
+function outputUsers(users){
+  userList.innerHTML=`
+    ${users.map(user=>`<li>${user.username}</li>`).join('')}
+  `;
 }
